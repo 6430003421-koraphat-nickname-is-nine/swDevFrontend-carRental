52ffb72e58e93e61a6c2eef0b243934f08a130bc
koraphat <koraphat.lum@gmail.com>
2024-10-04 18:14:13 +0700

    class9 fix code

--- src/libs/getCar.tsx
+++ src/libs/getCar.tsx
@@ -1,6 +1,3 @@
-import getCars from "./getCars";
-import CarCatalog from "@/components/CarCatalog";
-
 export default async function getCar(id: string) {
   const response = await fetch(`http://localhost:5000/api/v1/cars/${id}`);
   if (!response) {
